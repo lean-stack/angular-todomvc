632d6bf027b5bb6d603eb7e8f105e2c0127c61ba
Micha Alt <mail@lean-stack.rocks>
2020-01-05 21:49:42 +0100

    First component: todos shell. Exported of course.
    Genrated via
        ng g c todos/todos-shell --export

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TodosShellComponent } from './todos-shell/todos-shell.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TodosShellComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [TodosShellComponent]
 })
 export class TodosModule { }
