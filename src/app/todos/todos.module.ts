fe7a0f001c482aa7d42d523e1022285282c78233
Micha Alt <mail@lean-stack.rocks>
2020-01-05 22:00:57 +0100

    Refactoring: moved the components to an own folder.

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TodosShellComponent } from './todos-shell/todos-shell.component';
-import { TodosInputComponent } from './todos-input/todos-input.component';
-import { TodosMainComponent } from './todos-main/todos-main.component';
-import { TodosActionbarComponent } from './todos-actionbar/todos-actionbar.component';
-import { TodosListComponent } from './todos-list/todos-list.component';
-import { TodosItemComponent } from './todos-item/todos-item.component';
+import { TodosShellComponent } from './components/todos-shell/todos-shell.component';
+import { TodosInputComponent } from './components/todos-input/todos-input.component';
+import { TodosMainComponent } from './components/todos-main/todos-main.component';
+import { TodosActionbarComponent } from './components/todos-actionbar/todos-actionbar.component';
+import { TodosListComponent } from './components/todos-list/todos-list.component';
+import { TodosItemComponent } from './components/todos-item/todos-item.component';
 
 @NgModule({
   declarations: [TodosShellComponent, TodosInputComponent, TodosMainComponent, TodosActionbarComponent, TodosListComponent, TodosItemComponent],
