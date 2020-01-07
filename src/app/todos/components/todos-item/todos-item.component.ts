f98d26db438c7f2d5e8f20791824e250c66fd5aa
Micha Alt <mail@lean-stack.rocks>
2020-01-07 20:27:40 +0100

    Handle event by component method.

--- src/app/todos/components/todos-item/todos-item.component.ts
+++ src/app/todos/components/todos-item/todos-item.component.ts
@@ -16,4 +16,11 @@ export class TodosItemComponent implements OnInit {
     this.todo = { id: 17, title: 'Template Syntax', completed: false };
   }
 
+  toggleCompletedState() {
+    // TODO: Lokale Änderungen hier müssen an ein Backend weitergegeben werden.
+    // Besser noch: die Komponente sollte das Todo nur "lesen". Änderungen
+    // an zentraler Stelle druchführen
+    console.log('TODO: delegate mutating of data to central state store');
+    this.todo.completed = !this.todo.completed;
+  }
 }
