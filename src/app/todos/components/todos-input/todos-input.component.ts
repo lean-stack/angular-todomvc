9567a904258a4f3546244376e6185bde9e822266
Micha Alt <mail@lean-stack.rocks>
2020-01-07 22:00:16 +0100

    Input component component and template bindings.

--- src/app/todos/components/todos-input/todos-input.component.ts
+++ src/app/todos/components/todos-input/todos-input.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosInputComponent implements OnInit {
 
+  // Component state
+  title = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  titleEntered() {
+    if (this.title.trim().length > 0) {
+      const title = this.title.trim();
+      console.log('TODO: let someone create this todo item ;-)');
+      this.title = '';
+    }
+  }
 }
