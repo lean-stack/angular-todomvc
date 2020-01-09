28210e822114ca7b539a8f14f6587fbddced3c35
Micha Alt <mail@lean-stack.rocks>
2020-01-09 22:03:05 +0100

    Bug: not trimming edited todo title text. See #2.

--- src/app/todos/components/todos-item/todos-item.component.spec.ts
+++ src/app/todos/components/todos-item/todos-item.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TodosItemComponent } from './todos-item.component';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {Todo} from '../../models/todo';
+import {StoreService} from '../../state/store.service';
 
 describe('TodosItemComponent', () => {
   let component: TodosItemComponent;
@@ -10,9 +11,12 @@ describe('TodosItemComponent', () => {
 
   const todoMock: Todo = { id: 42, title: 'Find the question!', completed: false };
 
+  const storeServiceMock = jasmine.createSpyObj<StoreService>(['updateTodoTitle']);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TodosItemComponent ],
+      providers: [ { provide: StoreService, useValue: storeServiceMock } ],
       schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
@@ -27,5 +31,14 @@ describe('TodosItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should trim edited title text', () => {
+
+    component.beginEdit();
+    component.editText = '  Whitespace around  ';
+    component.endEdit();
+
+    expect(storeServiceMock.updateTodoTitle).toHaveBeenCalledWith(todoMock, 'Whitespace around');
   });
 });
