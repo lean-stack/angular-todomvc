9a97a347a024381d46a76432f870641ebc79bfa2
Micha Alt <mail@lean-stack.de>
2020-01-09 14:21:12 +0100

    Custom pipe: pluralize.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FocusOnDirective } from './directives/focus-on.directive';
+import { PluralizePipe } from './pipes/pluralize.pipe';
 
 
 
 @NgModule({
-  declarations: [FocusOnDirective],
+  declarations: [FocusOnDirective, PluralizePipe],
   imports: [
     CommonModule
   ],
-  exports: [
-    CommonModule,
-    FocusOnDirective
-  ]
+    exports: [
+        CommonModule,
+        FocusOnDirective,
+        PluralizePipe
+    ]
 })
 export class SharedModule { }
