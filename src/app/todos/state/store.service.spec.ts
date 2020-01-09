0c62a57c1d3ab858220da158bd11ff8983fee30a
Micha Alt <mail@lean-stack.rocks>
2020-01-09 22:25:15 +0100

    Bug: not deleting all completed todos. See #3

--- src/app/todos/state/store.service.spec.ts
+++ src/app/todos/state/store.service.spec.ts
@@ -5,8 +5,27 @@ import { StoreService } from './store.service';
 describe('StoreService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
+  let service: StoreService;
+
+  beforeEach(() => {
+    service = TestBed.get(StoreService);
+  });
   it('should be created', () => {
-    const service: StoreService = TestBed.get(StoreService);
     expect(service).toBeTruthy();
+  });
+
+  it('should delete all completed todos', () => {
+    localStorage.clear();
+    service.createTodo('One');
+    service.createTodo('Two');
+    service.createTodo('Three');
+    service.createTodo('Four');
+    service.toggleTodoCompletedState(service.state.todos[0]);
+    service.toggleTodoCompletedState(service.state.todos[1]);
+    service.toggleTodoCompletedState(service.state.todos[3]);
+
+    service.destroyAllCompletedTodos();
+
+    expect(service.state.todos.length).toBe(1);
   });
 });
